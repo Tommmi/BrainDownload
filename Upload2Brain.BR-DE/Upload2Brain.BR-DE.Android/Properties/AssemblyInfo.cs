4c1d28e9ac891f31e2440ae6924d96a5d1adbd37
Thomas <thomas@gerlach-software.de>
2023-03-25 18:58:37 +0100

    branch created
    branch created

--- Upload2Brain.BR-DE/Upload2Brain.BR-DE.Android/Properties/AssemblyInfo.cs
+++ Upload2Brain.BR-DE/Upload2Brain.BR-DE.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Upload2Brain.BR_DE.Android")]
+[assembly: AssemblyTitle("Download2Brain.EN_Tom.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Upload2Brain.BR_DE.Android")]
+[assembly: AssemblyProduct("Download2Brain.EN_Tom.Android")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
