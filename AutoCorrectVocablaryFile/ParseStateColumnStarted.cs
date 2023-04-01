fce79fa6dbe4131db3b806f4adbee95823897c3a
Thomas <thomas@gerlach-software.de>
2023-04-01 19:01:45 +0200

    AutoCorrectVocabularyFile

--- AutoCorrectVocablaryFile/ParseStateColumnStarted.cs
+++ AutoCorrectVocablaryFile/ParseStateColumnStarted.cs
@@ -17,25 +17,38 @@ namespace AutoCorrectVocablaryFile
                     {
                         context.NewContent.AppendLine();
                         context.Column = 0;
+                        context.Guid = string.Empty;
                     }
                     else
                     {
                         context.NewContent.Append(c);
                         context.NewContent.AppendLine("ERROR - 276574");
+                        throw new Exception("line feed at wrong column");
                     }
                     break;
                 case '\r':
                     if (context.Column < 2)
                     {
                         context.NewContent.Append("ERROR - 9037453");
+                        throw new Exception("line feed at wrong column");
                     }
                     break;
                 case ';':
+                    if(context.Column==0)
+                    {
+                        if(context.Guid!="ID"
+                           && !Guid.TryParse(context.Guid, out var guid))
+                        {
+                            context.NewContent.AppendLine("ERROR - 8793fj834");
+                            throw new Exception("missing guid");
+                        }
+                    }
                     context.NewContent.Append(c);
                     context.Column++;
                     if (context.Column > 2)
                     {
                         context.NewContent.AppendLine("ERROR - n56z54");
+                        throw new Exception("too much columns");
                     }
                     break;
                 case '\"':
@@ -44,6 +57,10 @@ namespace AutoCorrectVocablaryFile
                     break;
                 default:
                     context.NewContent.Append(c);
+                    if(context.Column == 0)
+                    {
+                        context.Guid += c;
+                    }
                     break;
             }
         }
