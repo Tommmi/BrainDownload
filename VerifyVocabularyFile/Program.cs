a77842aeca7eaf006c95ca77a1ac87239e961aea
Thomas <thomas@gerlach-software.de>
2024-05-05 10:07:52 +0200

    .

--- VerifyVocabularyFile/Program.cs
+++ VerifyVocabularyFile/Program.cs
@@ -9,6 +9,14 @@ var userRepository = new UserRepository(validate:true);
 try
 {
     var words = await userRepository.LoadAllWords();
+    if(words.GroupBy(w=>w.Id).Any(g=>g.Count()!=1))
+    {
+        foreach(var word in words.GroupBy(w => w.Id).First(g => g.Count() != 1))
+        {
+            Console.WriteLine($"##############\nERROR\n##############\nin id:{word.Id}");
+            return;
+        }
+    }
     Console.WriteLine("succeeded");
 }
 catch (Exception e)
